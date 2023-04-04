896c045d65edc3260f2cbc927fcb9a98995d5543
Your Nameanother138145800000 <abolfazlchavoshi1381@gmail.com>
2023-04-05 01:56:44 +0330

    Improvment: 1. add check status 2. use swich

--- src/MazeGameDFS.java
+++ src/MazeGameDFS.java
@@ -11,17 +11,21 @@ public class MazeGameDFS {
     private static ArrayList<String> path;
     private static int startRow;
     private static int startCol;
-    private static int endRow;
-    private static int endCol;
+    private static int goalRow;
+    private static int goalCol;
+    private static String status; // start is ??? goal
 
     public static void main(String[] args) throws IOException {
         String filename = "Maze_env.txt";
         readMazeFile(filename);
 
+        checkStatus();
+
         long startTime = System.nanoTime();
         boolean solved = dfsSolver(startRow, startCol);
         long endTime = System.nanoTime();
         double timeElapsed = (endTime - startTime) / 1e6;  // convert to milliseconds
+        double time = endTime - startTime;  // convert to milliseconds
 
         if (solved) {
             printPath();
@@ -32,6 +36,7 @@ public class MazeGameDFS {
             System.out.println("Time elapsed: " + timeElapsed + " ms");
         } else {
             System.out.println("No solution found");
+            System.out.print("-------------------------------------");
         }
     }
 
@@ -55,56 +60,184 @@ public class MazeGameDFS {
                     startCol = j;
                 }
                 if (maze[i][j].equals("G")) {
-                    endRow = i;
-                    endCol = j;
+                    goalRow = i;
+                    goalCol = j;
                 }
             }
         }
         reader.close();
     }
 
+    private static void checkStatus() {
+        if (startRow > goalRow && startCol > goalCol) { // start is down right goal
+            status = "down right";
+        }
+
+        if (startRow > goalRow && startCol < goalCol) {// start is down left goal
+            status = "down left";
+        }
+        if (startRow < goalRow && startCol > goalCol) {// start is up right goal
+            status = "up right";
+        }
+        if (startRow < goalRow && startCol < goalCol) {// start is up left goal
+            status = "up left";
+        }
+        if (startRow == goalRow) {
+            if (startCol > goalCol)
+                status = "right";
+            else
+                status = "left";
+        }
+        if (startCol == goalCol)
+            if (startRow > goalRow)
+                status = "down";
+            else
+                status = "up";
+    }
+
     private static boolean dfsSolver(int currRow, int currCol) {
-        if (currRow < 0 || currRow >= numRows || currCol < 0 || currCol >= numCols) {
-            return false;  // out of range
-        }
-
-        if (maze[currRow][currCol].equals("%")) {
-            return false;  // hit obstacle
-        }
-
-        if (visited[currRow][currCol]) {
-            return false;  // already visited
+        if ((currRow < 0 || currRow >= numRows || currCol < 0 || currCol >= numCols) // out of range
+                || (maze[currRow][currCol].equals("%") // hit obstacle
+                || (visited[currRow][currCol]))) { // already visited
+            return false;
         }
 
         visited[currRow][currCol] = true;
 
-        if (currRow == endRow && currCol == endCol) {
+        if (currRow == goalRow && currCol == goalCol) {
             return true;  // reached the end
         }
 
+        boolean isMoveUp;
+        boolean isMoveDown;
+        boolean isMoveRight;
+        boolean isMoveLeft;
+
+        switch (status) {
+            case "down right":
+                isMoveUp = moveUp(currRow, currCol);
+                if (isMoveUp) {
+                    return true;
+                }
+                isMoveLeft = moveLeft(currRow, currCol);
+                if (isMoveLeft) {
+                    return true;
+                }
+                isMoveRight = moveRight(currRow, currCol);
+                if (isMoveRight) {
+                    return true;
+                }
+                isMoveDown = moveDown(currRow, currCol);
+                if (isMoveDown) {
+                    return true;
+                }
+                break;
+            case "down left":
+                isMoveUp = moveUp(currRow, currCol);
+                if (isMoveUp) {
+                    return true;
+                }
+                isMoveRight = moveRight(currRow, currCol);
+                if (isMoveRight) {
+                    return true;
+                }
+                isMoveLeft = moveLeft(currRow, currCol);
+                if (isMoveLeft) {
+                    return true;
+                }
+                isMoveDown = moveDown(currRow, currCol);
+                if (isMoveDown) {
+                    return true;
+                }
+                break;
+            case "up right":
+                isMoveDown = moveDown(currRow, currCol);
+                if (isMoveDown) {
+                    return true;
+                }
+                isMoveLeft = moveLeft(currRow, currCol);
+                if (isMoveLeft) {
+                    return true;
+                }
+                isMoveRight = moveRight(currRow, currCol);
+                if (isMoveRight) {
+                    return true;
+                }
+                isMoveUp = moveUp(currRow, currCol);
+                if (isMoveUp) {
+                    return true;
+                }
+                break;
+            case "up left":
+                isMoveDown = moveDown(currRow, currCol);
+                if (isMoveDown) {
+                    return true;
+                }
+                isMoveRight = moveRight(currRow, currCol);
+                if (isMoveRight) {
+                    return true;
+                }
+                isMoveLeft = moveLeft(currRow, currCol);
+                if (isMoveLeft) {
+                    return true;
+                }
+                isMoveRight = moveUp(currRow, currCol);
+                if (isMoveRight) {
+                    return true;
+                }
+                break;
+            case "right":
+                moveLeft(currRow, currCol);
+                return true;
+            case "left":
+                moveRight(currRow, currCol);
+                return true;
+            case "up":
+                moveDown(currRow, currCol);
+                return true;
+            case "down":
+                moveUp(currRow, currCol);
+                return true;
+            default:
+                return false;// no path found
+        }
+        return false;// no path found
+    }
+
+    private static boolean moveUp(int currRow, int currCol) {
         if (dfsSolver(currRow - 1, currCol)) {  // move up
             maze[currRow][currCol] = "↑";
             path.add("U");
             return true;
-        }
+        } else
+            return false;
+    }
+
+    private static boolean moveDown(int currRow, int currCol) {
         if (dfsSolver(currRow + 1, currCol)) {  // move down
             maze[currRow][currCol] = "↓";
             path.add("D");
             return true;
-        }
+        } else
+            return false;
+    }
 
+    private static boolean moveRight(int currRow, int currCol) {
         if (dfsSolver(currRow, currCol + 1)) {  // move right
             maze[currRow][currCol] = "→";
             path.add("R");
             return true;
-        }
+        } else
+            return false;
+    }
+
+    private static boolean moveLeft(int currRow, int currCol) {
         if (dfsSolver(currRow, currCol - 1)) {  // move left
             maze[currRow][currCol] = "←";
             path.add("L");
             return true;
-        }
-
-        return false;  // no path found
+        } else
+            return false;
     }
 
     private static void printPath() {
@@ -120,7 +253,7 @@ public class MazeGameDFS {
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
                 if (maze[i][j].equals("S") || maze[i][j].equals("%") || maze[i][j].equals("-")) {
-                    maze[i][j] = "";
+                    maze[i][j] = "×";
                 }
             }
         }
